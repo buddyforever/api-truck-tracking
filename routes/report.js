916b7367ab158dcdd66b920f281050c49f69b244
Andrei Iunda <buddyforever.dev@gmail.com>
2020-12-26 06:14:58 +0700

    reports for summary

--- routes/report.js
+++ routes/report.js
@@ -3,17 +3,63 @@ var router = express.Router();
 
 var db = require("../db");
 
-router.get("/getDailyTruckNum", (req, res) => {
+router.get("/getDailyTruckNum/:cid", (req, res) => {
+  var companyId = req.params.cid;
   var queryTableNumLoaded =
-    "SELECT dates.date, COUNT(*) as numTruckLoaded FROM submit_logs sl JOIN (SELECT submitDate as date FROM submit_logs GROUP BY submitDate) dates ON sl.submitDate=dates.date WHERE sl.dealStatus=2 GROUP BY dates.date";
+    "SELECT dates.date, COUNT(*) as numTruckLoaded FROM submit_logs sl JOIN (SELECT submitDate as date FROM submit_logs GROUP BY submitDate) dates ON sl.submitDate=dates.date JOIN deals d ON sl.dealId=d.id WHERE sl.dealStatus=2 AND d.companyId=" +
+    companyId +
+    " GROUP BY dates.date";
   var queryTableNumUnloaded =
-    "SELECT dates.date, COUNT(*) as numTruckUnLoaded FROM submit_logs sl JOIN (SELECT submitDate as date FROM submit_logs GROUP BY submitDate) dates ON sl.submitDate=dates.date WHERE sl.dealStatus=4 GROUP BY dates.date";
+    "SELECT dates.date, COUNT(*) as numTruckUnLoaded FROM submit_logs sl JOIN (SELECT submitDate as date FROM submit_logs GROUP BY submitDate) dates ON sl.submitDate=dates.date JOIN deals d ON sl.dealId=d.id WHERE sl.dealStatus=4 AND d.companyId=" +
+    companyId +
+    " GROUP BY dates.date";
   var query =
     "SELECT DATE_FORMAT(tbl1.date, '%Y-%m-%d') as date, tbl1.numTruckLoaded, tbl2.numTruckUnloaded FROM (" +
     queryTableNumLoaded +
     ") as tbl1 JOIN (" +
     queryTableNumUnloaded +
-    ") tbl2 ON tbl1.date=tbl2.date";
+    ") tbl2 ON tbl1.date=tbl2.date ORDER BY date DESC ";
+  db.query(query, function (error, results, fields) {
+    if (error) throw error;
+    if (results.length > 0)
+      res.send({
+        status: 200,
+        result: results,
+      });
+  });
+});
+router.get("/getMonthlyDeliveryTime/:cid", (req, res) => {
+  var companyId = req.params.cid;
+  var year = new Date().getFullYear();
+  var query =
+    "SELECT MONTH(startUnloadingAt) as month, AVG(TIMESTAMPDIFF(SECOND, finishLoadingAt, startUnloadingAt)) as avg_delievery_time FROM deals WHERE status>=3 AND companyId=" +
+    companyId +
+    " AND YEAR(startUnloadingAt)='" +
+    year +
+    "' GROUP BY MONTH(startUnloadingAt)";
+  db.query(query, function (error, results, fields) {
+    if (error) throw error;
+    if (results.length > 0)
+      res.send({
+        status: 200,
+        result: results,
+      });
+  });
+});
+router.get("/getMonthlyNetLoss/:cid", (req, res) => {
+  var companyId = req.params.cid;
+  var year = new Date().getFullYear();
+  var query = "";
+  if (companyId == 1)
+    query =
+      "SELECT MONTH(finishUnloadingAt) as month, AVG(netWeight-newNetWeight) as netLoss FROM deals WHERE status>=3 AND companyId=1 AND status=4 AND YEAR(finishUnloadingAt)='" +
+      year +
+      "' GROUP BY MONTH(finishUnloadingAt)";
+  if (companyId == 2)
+    query =
+      "SELECT MONTH(finishUnloadingAt) as month, AVG(quantity-newQuantity) as netLoss FROM deals WHERE status>=3 AND companyId=2 AND status=4 AND YEAR(finishUnloadingAt)='" +
+      year +
+      "' GROUP BY MONTH(finishUnloadingAt)";
   db.query(query, function (error, results, fields) {
     if (error) throw error;
     if (results.length > 0)
