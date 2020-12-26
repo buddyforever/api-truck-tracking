05b37b93d8633fc40bd127509a0fb399ee7479fd
Andrei Iunda <buddyforever.dev@gmail.com>
2020-12-27 04:22:01 +0700

    save user's ip and login time

---
+++ cronjob.js
@@ -0,0 +1,13 @@
+// var CronJob = require("cron").CronJob;
+
+// var job = new CronJob(
+//   "* * * * * *",
+//   function () {
+//     console.log("You will see this messave every second");
+//   },
+//   null,
+//   true,
+//   "America/Los_Angeles"
+// );
+
+// module.exports = job;
