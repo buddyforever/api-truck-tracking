3268827067760699d0a550b3936f1e37ab06669e
Samuel <buddyforever.dev@gmail.com>
2021-02-03 01:59:35 -0300

    update request header

--- server.js
+++ server.js
@@ -26,11 +26,8 @@ app.use(cors());
 app.use(function (req, res, next) {
   //set headers to allow cross origin request.
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Headers", "*");
   next();
 });
 
